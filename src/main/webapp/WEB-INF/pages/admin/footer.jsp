5526386e392d162ff8fd5c6c160d2b9e051fb315
Huynh Hiep Si <sihuynh.tkh@gmail.com>
2017-12-21 23:19:26 +0700

    add view

--- src/main/webapp/WEB-INF/pages/admin/footer.jsp
+++
@@ -1,3 +0,0 @@
-<footer>
-	<p>© 2017 - WebserviceX.NET</p>
-</footer>
--- src/main/webapp/resources/js/common.js
+++
@@ -1,9 +0,0 @@
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
-
-$(document).ready(function() {
-    $("input[type='number']").on("keypress", function() {
-
-    })
-})
