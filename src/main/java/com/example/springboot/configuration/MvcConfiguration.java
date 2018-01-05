e896710b9f463fe26f74a237e9260d5a906a9ea0
si.huynh <si.huynh@sutrixsolutions.com>
2018-01-05 18:02:50 +0700

    springboot run in external tomcat

--- src/main/java/com/example/springboot/configuration/MvcConfiguration.java
+++ src/main/java/com/example/springboot/configuration/MvcConfiguration.java
@@ -19,7 +19,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/");
+		resolver.setPrefix("/WEB-INF/view/");
 		resolver.setSuffix(".jsp");
 		resolver.setViewClass(JstlView.class);
 		registry.viewResolver(resolver);
