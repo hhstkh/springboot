13dc5a18863201011e25534fd80d949cdc9560a6
hhstkh <sihuynh.tkh@gmail.com>
2018-01-07 22:38:21 +0700

    ftl

--- src/main/java/com/example/springboot/configuration/MvcConfiguration.java
+++ src/main/java/com/example/springboot/configuration/MvcConfiguration.java
@@ -6,24 +6,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class MvcConfiguration extends WebMvcConfigurerAdapter {
 
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/view/");
-		resolver.setSuffix(".jsp");
-		resolver.setViewClass(JstlView.class);
-		registry.viewResolver(resolver);
+//	@Override
+//	public void configureViewResolvers(ViewResolverRegistry registry) {
+//		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//		resolver.setPrefix("/WEB-INF/view/");
+//		resolver.setSuffix(".jsp");
+//		resolver.setViewClass(JstlView.class);
+//		registry.viewResolver(resolver);
+//	}
+	
+	/*@Bean 
+	public FreeMarkerViewResolver freemarkerViewResolver() { 
+	    FreeMarkerViewResolver resolver = new FreeMarkerViewResolver(); 
+	    resolver.setCache(true); 
+	    resolver.setPrefix(""); 
+	    resolver.setSuffix(".ftl"); 
+	    return resolver; 
 	}
+	
+	@Bean 
+	public FreeMarkerConfigurer freemarkerConfig() { 
+	    FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer(); 
+	    freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/views/ftl/");
+	    return freeMarkerConfigurer; 
+	}*/
 	
 	@Bean
 	public ErrorPageFilter errorPageFilter() {
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,16 @@
+<#import "/spring.ftl" as spring/>
+ 
+<!DOCTYPE HTML>
+<html>
+   <head>
+      <meta charset="UTF-8" />
+      <title>Welcome</title>
+      
+   </head>
+    
+   <body>
+     Hello FTL
+       
+   </body>
+    
+</html>
