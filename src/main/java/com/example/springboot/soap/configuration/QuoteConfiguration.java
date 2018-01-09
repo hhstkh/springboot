8b8343c4c7a4dbc1952c967734e56a20ad536339
si.huynh <si.huynh@sutrixsolutions.com>
2018-01-09 15:56:50 +0700

    spring webservice

---
+++ src/main/java/com/example/springboot/soap/configuration/QuoteConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.springboot.soap.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import com.example.spring.soap.client.QuoteClient;
+
+@Configuration
+public class QuoteConfiguration {
+
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		// this package must match the package in the <generatePackage> specified in
+		// pom.xml
+		marshaller.setContextPath("com.example.springboot.soap.quote");
+		return marshaller;
+	}
+
+	@Bean
+	public QuoteClient quoteClient(Jaxb2Marshaller marshaller) {
+		QuoteClient client = new QuoteClient();
+		client.setDefaultUri("http://www.webservicex.com/stockquote.asmx");
+		client.setMarshaller(marshaller);
+		client.setUnmarshaller(marshaller);
+		return client;
+	}
+
+}
